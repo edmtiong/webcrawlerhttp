f06fc8468d35eeafe202cab0f70c69eafeec16b1
edmtiong <edmund.tiong1@gmail.com>
2024-05-25 19:46:33 +0800

    included test cases for normalizeURL and getURLfrom HTML

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const {JSDOM} = require('jsdom')
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -5,9 +7,36 @@ function normalizeURL(urlString) {
         return hostPath.slice(0,-1) /*return all except last character*/
     } 
     return hostPath
-    
 }
 
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    
+    for (const linkElement of linkElements){
+        if(linkElement.href.slice(0,1) === '/'){
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.log(`error message: ${err.message}`)
+            }
+        } else{ 
+            //absolute
+            try{   
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.log(`error message: ${err.message}`)
+            }
+        }
+       
+    }
+    return urls
+}
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
