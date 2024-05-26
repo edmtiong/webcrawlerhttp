23328618edb7a28e8fcdcd46b7f0dff669acbef0
edmtiong <edmund.tiong1@gmail.com>
2024-05-26 10:43:54 +0800

    include pageCrawl and try and catch

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling ${currentURL}`)
+
+    try{
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (contentType != "text/html") {
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -38,5 +62,6 @@ function getURLsFromHTML(htmlBody, baseURL){
 }
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
